e519fa877abc3df77a0a968681109fd3bd7381ee
RemyYYZ <yang20080413@outlook.com>
2025-06-20 09:37:54 +0800

    Change 404

--- app/error.vue
+++ app/error.vue
@@ -1,15 +1,16 @@
 <script setup>
+definePageMeta({
+  layout: 'empty'
+})
 import errorImage from './assets/images/404.svg?raw'
 </script>
 
 <template>
-  <NuxtLayout name="empty">
-    <h1 class="text-3xl font-bold mb-2">页面未找到</h1>
-    <p class="text-gray-500 mb-6">抱歉，您访问的链接不存在，可能已失效或被删除。你可以点击下面的图片返回主页。</p>
-    <a
-      class="w-full h-full max-w-[600px] mx-auto flex items-center justify-center [&_svg]:w-full"
-      href="/"
-      v-html="errorImage"
-    />
-  </NuxtLayout>
+  <h1 class="text-3xl font-bold mb-2">页面未找到</h1>
+  <p class="text-gray-500 mb-6">抱歉，您访问的链接不存在，可能已失效或被删除。你可以点击下面的图片返回主页。</p>
+  <a
+    class="w-full h-full max-w-[600px] mx-auto flex items-center justify-center [&_svg]:w-full"
+    href="/"
+    v-html="errorImage"
+  />
 </template>
