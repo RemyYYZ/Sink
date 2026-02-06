6f7141a21fbf55be2a2050208bd2a5521dba2249
RemyYYZ <yang20080413@outlook.com>
2026-02-06 21:03:19 +0800

    Change 404

--- app/error.vue
+++ app/error.vue
@@ -6,7 +6,8 @@ import errorImage from './assets/images/404.svg?raw'
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold mb-2">页面未找到</h1>
+  <h1 class="text-4xl font-bold mb-2">MCAPPX版本库下载链接解析服务</h1>
+  <h1 class="text-3xl font-bold mb-2">错误：无效链接</h1>
   <p class="text-gray-500 mb-6">抱歉，您访问的链接不存在，可能已失效或被删除。你可以点击下面的图片返回主页。</p>
   <a
     class="w-full h-full max-w-[600px] mx-auto flex items-center justify-center [&_svg]:w-full"
