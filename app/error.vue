72a72c7fe892ca0fb22c521b346bd1dcdfc52ce5
RemyYYZ <yang20080413@outlook.com>
2026-04-04 11:39:00 +0800

    Change Pages

--- app/error.vue
+++ app/error.vue
@@ -1,21 +1,17 @@
-<script setup lang="ts">
-import type { NuxtError } from '#app'
+<script setup>
+definePageMeta({
+  layout: 'empty'
+})
 import errorImage from './assets/images/404.svg?raw'
-
-defineProps<{
-  error: NuxtError
-}>()
 </script>
 
 <template>
-  <NuxtLayout name="default">
-    <a
-      class="
-        mx-auto flex h-full w-full max-w-[600px] items-center justify-center
-        [&_svg]:w-full
-      "
-      href="/"
-      v-html="errorImage"
-    />
-  </NuxtLayout>
+  <h1 class="text-4xl font-bold mb-2">MCAPPX版本库下载链接解析服务</h1>
+  <h1 class="text-3xl font-bold mb-2">错误：无效链接</h1>
+  <p class="text-gray-500 mb-6">抱歉，您访问的链接不存在，可能已失效或被删除。你可以点击下面的图片返回主页。</p>
+  <a
+    class="w-full h-full max-w-[600px] mx-auto flex items-center justify-center [&_svg]:w-full"
+    href="/"
+    v-html="errorImage"
+  />
 </template>
